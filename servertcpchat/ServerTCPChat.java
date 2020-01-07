a25c3ef47875e8860f97759826669e418b0ec735
Alessio-Hu-Peano-5C <56298451+Alessio-Hu-Peano-5C@users.noreply.github.com>
2020-01-07 11:00:01 +0100

    Add files via upload

---
+++ servertcpchat/ServerTCPChat.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servertcpchat;
+
+import java.io.IOException;
+import java.net.*;
+
+/**
+ *
+ * @author hu.alessio
+ */
+public class ServerTCPChat {
+
+    public static void main(String[] args) {
+
+        int porta = 1234;
+
+        try {
+            ServerSocket server = new ServerSocket(porta);
+            System.out.println("Server chatTCP in esecuzione...");
+
+            while (true) {
+                Users w;
+                try {
+                    Socket s = server.accept();
+                    w = new Users(s);
+                    Thread t = new Thread(w);
+                    t.start();
+                } catch (IOException e) {
+                    System.out.println("Connessione NON riuscita con client: ");
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Error! Porta: " + porta + " non disponibile");
+        }
+
+    }
+
+}
